108cb6ed6a02783e44b9e8e418694ad4f0d8d34d
Andreas <git@fsck.rip>
2024-11-02 15:01:05 -0400

    remove lab-specific code
    Signed-off-by: Andreas <git@fsck.rip>

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import csv
 import win32net
 import win32netcon
 import argparse
-    
+
+
 def main(args):
-    with open('./userpass.csv') as csvfile:
+    with open("./userpass.csv") as csvfile:
         userpassreader = csv.DictReader(csvfile, delimiter=",")
         count = 0
-        
+
         group = None
-        if args.mode == "domain":   
-            group = f"{args.group}.lab.tu"
-           
+        if args.mode == "domain":
+            group = args.group
+
         if args.mode == "domain" and args.group is None:
-            raise Exception("You must provide a group name with domain mode")
-            
+            raise Exception("You must provide a group name with domain mode (e.g. group-14.lab.tu)")
+
         for row in userpassreader:
             try:
                 # Delete the user first (if they exist) and create again.
@@ -24,26 +25,32 @@ def main(args):
                 win32net.NetUserDel(group, row["username"])
             except win32net.error:
                 pass
-            
+
             try:
-                win32net.NetUserAdd(group, 1, dict (
-                    name = row["username"],
-                    password = row["password"],
-                    priv = win32netcon.USER_PRIV_USER,
-                    flags = win32netcon.UF_SCRIPT,
-                    comment = "added from script"
-                ))      
-                
-                print(f"Created user {row["username"]}, with password {row["password"]} to {args.mode} users")
+                win32net.NetUserAdd(
+                    group,
+                    1,
+                    dict(
+                        name=row["username"],
+                        password=row["password"],
+                        priv=win32netcon.USER_PRIV_USER,
+                        flags=win32netcon.UF_SCRIPT,
+                        comment="added from script",
+                    ),
+                )
+
+                print(
+                    f"Created user {row["username"]}, with password {row["password"]} to {args.mode} users"
+                )
                 count += 1
             except win32net.error as e:
                 print(e)
-                
-        
+
         print(f"Added a total of {count} users")
-        
+
+
 if __name__ == "__main__":
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
-    parser.add_argument("mode", type=str, default="local", choices=['local', 'domain'])
+    parser.add_argument("mode", type=str, default="local", choices=["local", "domain"])
     parser.add_argument("--group", type=str)
     main(parser.parse_args())
